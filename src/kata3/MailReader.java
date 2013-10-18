c01697a20fbee21d24ce2de0c0788b7db7d20d3d
usuario <usuario@10.22.146.202>
2013-10-18 10:54:44 +0100

    fifth

--- src/kata3/MailReader.java
+++ src/kata3/MailReader.java
@@ -7,7 +7,7 @@ public class MailReader {
     public static ArrayList<String> readDomains(String filename){
         ArrayList<String> list = new ArrayList<>();
         try{
-            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            BufferedReader reader = new BufferedReader(new FileReader(new File(filename)));
             while(true){
                 String mail = reader.readLine();
             }
