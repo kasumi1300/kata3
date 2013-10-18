b465c4606a1664d43c511c5563544d9262f9abce
usuario <usuario@10.22.146.202>
2013-10-18 11:23:26 +0100

    fifth

--- src/kata3/Main.java
+++ src/kata3/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Control control = new Control(args [0]);
         control.execute();
-        String filename = args[0];
        
     }
 }
