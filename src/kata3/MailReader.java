197a931a92c30a354945da1abca5a15205afc860
usuario <usuario@10.22.146.202>
2013-10-18 10:51:08 +0100

    fifth

--- src/kata3/MailReader.java
+++ src/kata3/MailReader.java
@@ -1,11 +1,24 @@
 
 package kata3;
-
+import java.io.*;
 import java.util.*;
 
 public class MailReader {
-    public static ArrayList<String> read(String filename){
-        return null;
+    public static ArrayList<String> readDomains(String filename){
+        ArrayList<String> list = new ArrayList<>();
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            while(true){
+                String mail = reader.readLine();
+            }
+        }
+        catch(FileNotFoundException ex){
+            
+        }
+        catch(IOException ex){
+            
+        }
+        return list;
     }
     
 }
