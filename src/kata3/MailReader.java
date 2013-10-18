c1dcf96970ce1e43c885f82e33d2687c6fff2028
usuario <usuario@10.22.146.202>
2013-10-18 10:42:50 +0100

    third

---
+++ src/kata3/MailReader.java
@@ -0,0 +1,11 @@
+
+package kata3;
+
+import java.util.*;
+
+public class MailReader {
+    public static ArrayList<String> read(String filename){
+        return null;
+    }
+    
+}
--- src/kata3/Main.java
+++ src/kata3/Main.java
@@ -4,9 +4,9 @@ package kata3;
 public class Main {
 
     public static void main(String[] args) {
-        HistogramBuilder<Integer> builder = new HistogramBuilder<>();
-        builder.calculate(new Integer[]{1,2,3,46,1,2,9,0,34,2,24,7,2,1,35,7});
-        HistogramViewer<Integer> viewer = new HistogramViewer<>(builder.getHistogram());
+        HistogramBuilder<String> builder = new HistogramBuilder<>();
+        builder.calculate(MailReader.read(""));
+        HistogramViewer<String> viewer = new HistogramViewer<>(builder.getHistogram());
         viewer.show();
     }
 }
