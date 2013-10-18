23b9701a32079f444f617f98d65b57a34bf0495f
usuario <usuario@10.22.146.202>
2013-10-18 10:46:10 +0100

    fourth

--- src/kata3/Main.java
+++ src/kata3/Main.java
@@ -2,8 +2,10 @@ package kata3;
 
 
 public class Main {
-
+//http://rectan.es.tl/lista-de-correos-electronicos.htm
+    
     public static void main(String[] args) {
+        String filename = args[0];
         HistogramBuilder<String> builder = new HistogramBuilder<>();
         builder.calculate(MailReader.read(""));
         HistogramViewer<String> viewer = new HistogramViewer<>(builder.getHistogram());
